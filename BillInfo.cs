4e7ecf6926fcb71e3fe5aa08dfbe824cd8b69331
Nguyễn Sỹ Tuấn <132974082+sytuan0712@users.noreply.github.com>
2023-10-30 12:21:05 +0700

    DTO

---
+++ BillInfo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DTO
+{
+    public class BillInfo
+    {
+        public BillInfo(int id,int count, int foodID, int billID)
+        {
+            this.Id = id;
+            this.Count = count;
+            this.FoodID = foodID;
+            this.BillID = billID;
+        }
+
+        public BillInfo(DataRow row)
+        {
+            this.Id = (int)row["id"];
+            this.BillID = (int)row["idBill"];
+            this.FoodID = (int)row["idFood"];
+            this.Count = (int)row["count"];
+        }
+
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        private int foodID;
+        public int FoodID
+        {
+            get { return foodID; }
+            set { foodID = value; }
+        }
+
+        private int billID;
+        public int BillID
+        {
+            get { return billID; }
+            set { billID = value; }
+        }
+
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+    }
+}
---
+++ Food.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DTO
+{
+    public class Food
+    {
+        public Food(int id, string name, float price, int categoryID)
+        {
+            this.CategoryID = categoryID;
+            this.Id = id;
+            this.Price = price;
+            this.Name = name;
+        }
+
+        public Food(DataRow row)
+        {
+            this.Id = (int)row["id"];
+            this.Name = row["name"].ToString();
+            this.CategoryID = (int)row["idcategory"];
+            this.Price = (float)Convert.ToDouble(row["price"].ToString());
+        }
+
+        private float price;
+        public float Price 
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        private int categoryID;
+        public int CategoryID
+        {
+            get { return categoryID; }
+            set { categoryID = value; }
+        }
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
