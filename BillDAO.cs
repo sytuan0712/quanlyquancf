43e0c8ca364f71d1adbbf0c986ac179256b30121
Nguyễn Sỹ Tuấn <132974082+sytuan0712@users.noreply.github.com>
2023-10-30 11:47:33 +0700

    DAO

---
+++ BillDAO.cs
@@ -0,0 +1,54 @@
+﻿using _1.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAO
+{
+    public class BillDAO
+    {
+        private static BillDAO instance;
+        public static BillDAO Instance 
+        {
+            get
+            {
+                if (instance == null) 
+                    instance = new BillDAO();
+                return BillDAO.instance;
+            }
+            private set { BillDAO.instance = value; }
+        }
+
+        private BillDAO(){ }
+
+        public int GetUnCheckBillIDByTableID(int id)
+        {
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM dbo.Bill WHERE idTable =" + id + "AND status = 0");
+            if(data.Rows.Count > 0)
+            {
+                Bill bill = new Bill(data.Rows[0]);
+                return bill.Id; 
+            }
+            return -1;
+        } 
+        
+        public void InsertBill(int id) 
+        {
+            DataProvider.Instance.ExecuteNonQuery("exec USP_InsertBill @idTable ", new object[]{id});
+        }
+        public int GetMaxIDBill()
+        {
+            try
+            {
+               return (int)DataProvider.Instance.ExecuteScalar("SELECT MAX(id) FROM dbo.Bill");
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+    }  
+}
