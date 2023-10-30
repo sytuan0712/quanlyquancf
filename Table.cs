67173ca680d354f08f88b5afd008a2689b61552a
Nguyễn Sỹ Tuấn <132974082+sytuan0712@users.noreply.github.com>
2023-10-30 12:20:04 +0700

    Add files via upload

---
+++ Table.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DTO
+{
+    public class Table
+    {
+        public Table(int id, string name, string status)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Status = status;
+        }
+
+        public Table(DataRow row)
+        {
+            this.Id = (int)row["id"];
+            this.Name = row["name"].ToString();
+            this.Status = row["status"].ToString();
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+        public string  Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+    }
+}
